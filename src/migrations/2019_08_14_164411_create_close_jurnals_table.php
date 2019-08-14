cf6dc168b406755ef72da095fba4f058c8e51cec
directoryx <anggawijaya4567@gmail.com>
2019-08-14 23:48:19 +0700

    menambahkan tabel close jurnal

---
+++ src/migrations/2019_08_14_164411_create_close_jurnals_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCloseJurnalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('close_jurnals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('uuid');
+            $table->integer('id_branch')->nullable();
+            $table->integer('approve')->default(0);
+            $table->string('transactionnumber');
+            $table->dateTime('transactiondate');
+            $table->integer('year');
+            $table->string('accountcode');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('close_jurnals');
+    }
+}
