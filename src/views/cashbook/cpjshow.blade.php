aa2efc39587cee9c0150e53cda755e8b8b393368
directoryx <anggawijaya4567@gmail.com>
2019-08-29 01:12:10 +0700

    small revision in show cashbook

--- src/views/cashbook/cpjshow.blade.php
+++ src/views/cashbook/cpjshow.blade.php
@@ -39,7 +39,7 @@
                                 <i class="la la-gear"></i>
                             </span>
 
-                            @include('label::edit')
+                            @include('label::show')
 
                             <h3 class="m-portlet__head-text">
                                 Cashbook - Cash Payment Journal
