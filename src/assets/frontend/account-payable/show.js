7409d2e4086093f889d95a74c3d12ce2823393d9
ibnuprtma <pibnu86@gmail.com>
2019-11-01 11:05:50 +0700

    view create show account payable

---
+++ src/assets/frontend/account-payable/show.js
@@ -0,0 +1,189 @@
+let AccountPayable = {
+    init: function () {
+		let suuplier_invoice = $('.supplier_invoice_datatable').mDatatable({
+				data: {
+						type: 'remote',
+						source: {
+								read: {
+										method: 'GET',
+										url: '',
+										map: function (raw) {
+												let dataSet = raw;
+
+												if (typeof raw.data !== 'undefined') {
+														dataSet = raw.data;
+												}
+
+												return dataSet;
+										}
+								}
+						},
+						pageSize: 10,
+						serverPaging: !0,
+						serverSorting: !0
+				},
+				layout: {
+						theme: 'default',
+						class: '',
+						scroll: false,
+						footer: !1
+				},
+				sortable: !0,
+				filterable: !1,
+				pagination: !0,
+				search: {
+						input: $('#generalSearch')
+				},
+				toolbar: {
+						items: {
+								pagination: {
+										pageSizeSelect: [5, 10, 20, 30, 50, 100]
+								}
+						}
+				},
+				columns: [
+						{
+							field: '',
+							title: 'Transaction No.',
+							sortable: 'asc',
+							filterable: !1,
+						},
+						{
+							field: '',
+							title: 'Date',
+							sortable: 'asc',
+							filterable: !1,
+						},
+						{
+							field: '',
+							title: 'Currency',
+							sortable: 'asc',
+							filterable: !1,
+						},
+						{
+							field: '',
+							title: 'Exchange Rate',
+							sortable: 'asc',
+							filterable: !1,
+						},
+						{
+							field: '',
+							title: 'Total Amount',
+							sortable: 'asc',
+							filterable: !1,
+						},
+						{
+							field: '',
+							title: 'Paid Amount',
+							sortable: 'asc',
+							filterable: !1,
+						},
+						{
+							field: '',
+							title: 'Account Code',
+							sortable: 'asc',
+							filterable: !1,
+						},
+						{
+							field: '',
+							title: 'Amount to Pay',
+							sortable: 'asc',
+							filterable: !1,
+						},
+						{
+							field: '',
+							title: 'Exchange Rate Gap',
+							sortable: 'asc',
+							filterable: !1,
+						},
+						{
+							field: '',
+							title: 'Description',
+							sortable: 'asc',
+							filterable: !1,
+						}
+
+				]
+		});
+
+		let adjustment_datatable = $('.adjustment_datatable').mDatatable({
+				data: {
+						type: 'remote',
+						source: {
+								read: {
+										method: 'GET',
+										url: '',
+										map: function (raw) {
+												let dataSet = raw;
+
+												if (typeof raw.data !== 'undefined') {
+														dataSet = raw.data;
+												}
+
+												return dataSet;
+										}
+								}
+						},
+						pageSize: 10,
+						serverPaging: !0,
+						serverSorting: !0
+				},
+				layout: {
+						theme: 'default',
+						class: '',
+						scroll: false,
+						footer: !1
+				},
+				sortable: !0,
+				filterable: !1,
+				pagination: !0,
+				search: {
+						input: $('#generalSearch')
+				},
+				toolbar: {
+						items: {
+								pagination: {
+										pageSizeSelect: [5, 10, 20, 30, 50, 100]
+								}
+						}
+				},
+				columns: [
+						{
+							field: '',
+							title: 'Account Code',
+							sortable: 'asc',
+							filterable: !1,
+						},
+						{
+							field: '',
+							title: 'Account Name',
+							sortable: 'asc',
+							filterable: !1,
+						},
+						{
+							field: '',
+							title: 'Debet',
+							sortable: 'asc',
+							filterable: !1,
+						},
+						{
+							field: '',
+							title: 'Credit',
+							sortable: 'asc',
+							filterable: !1,
+						},
+						{
+							field: '',
+							title: 'Description',
+							sortable: 'asc',
+							filterable: !1,
+						}
+
+				]
+		});
+    }
+};
+
+jQuery(document).ready(function () {
+    AccountPayable.init();
+});
---
+++ src/views/account-payable/show.blade.php
@@ -0,0 +1,238 @@
+@extends('frontend.master')
+
+@section('content')
+<div class="m-subheader hidden">
+    <div class="d-flex align-items-center">
+        <div class="mr-auto">
+            <h3 class="m-subheader__title m-subheader__title--separator">
+                Account Payable
+            </h3>
+            <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
+                <li class="m-nav__item m-nav__item--home">
+                    <a href="" class="m-nav__link m-nav__link--icon">
+                        <i class="m-nav__link-icon la la-home"></i>
+                    </a>
+                </li>
+                <li class="m-nav__separator">
+                    -
+                </li>
+                <li class="m-nav__item">
+                    <a href="#" class="m-nav__link">
+                        <span class="m-nav__link-text">
+                            Account Payable
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+@include('cashbookview::coamodal')
+<div class="m-content">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="m-portlet">
+                <div class="m-portlet__head">
+                    <div class="m-portlet__head-caption">
+                        <div class="m-portlet__head-title">
+                            <span class="m-portlet__head-icon m--hide">
+                                <i class="la la-gear"></i>
+                            </span>
+
+                            @include('label::show')
+
+                            <h3 class="m-portlet__head-text">
+                                Account Payable
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="m-portlet m-portlet--mobile">
+                    <div class="m-portlet__body">
+                        <form id="SupplierInvoiceGRNForm">
+                            <div class="m-portlet__body">
+                                <div class="form-group m-form__group row ">
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Date  
+                                        </label>
+
+                                        @component('label::data-info')
+                                            @slot('text', 'generated')
+                                        @endcomponent
+                                    </div>
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Vendor  
+                                        </label>
+        
+                                        @component('label::data-info')
+                                            @slot('text', 'generated')
+                                        @endcomponent
+                                    </div>
+                                </div>
+                                <div class="form-group m-form__group row ">
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Account Code  
+                                        </label>
+
+                                        @component('label::data-info')
+                                            @slot('text', 'generated')
+                                        @endcomponent
+                                    </div>
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Account Name
+                                        </label>
+
+                                        @component('label::data-info')
+                                            @slot('text', 'generated')
+                                        @endcomponent
+                                    </div>
+                                </div>
+                                <div class="form-group m-form__group row ">
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Currency  
+                                        </label>
+
+                                        @component('label::data-info')
+                                            @slot('text', 'generated')
+                                        @endcomponent
+                                    </div>
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Exchange Rate 
+                                        <span id="requi" class="requi" style="font-weight: bold;color:red">*</span>
+                                        </label>
+
+                                        @component('label::data-info')
+                                            @slot('text', 'generated')
+                                        @endcomponent
+                                    </div>
+                                </div>
+                                <div class="form-group m-form__group row ">
+                                    <div class="col-sm-12 col-md-12 col-lg-12">
+                                        <label class="form-control-label">
+                                            Remark
+                                        </label>
+
+                                        @component('label::data-info')
+                                            @slot('text', 'generated')
+                                        @endcomponent
+                                    </div>
+                                </div>
+                                {{-- datatables --}}
+                                <div class="form-group m-form__group row">
+                                    <div class="col-sm-12 col-md-12 col-lg-12">
+                                        <div class="m-portlet m-portlet--mobile">
+                                            <div class="m-portlet__body">
+                                                <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+                                                    <div class="row align-items-center">
+                                                        <div class="col-xl-6 order-2 order-xl-1">
+                                                            <div class="form-group m-form__group row align-items-center">
+                                                                <div class="col-md-6">
+                                                                    <div class="m-input-icon m-input-icon--left">
+                                                                        <input type="text" class="form-control m-input" placeholder="Search..."
+                                                                            id="generalSearch">
+                                                                        <span class="m-input-icon__icon m-input-icon__icon--left">
+                                                                            <span><i class="la la-search"></i></span>
+                                                                        </span>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-xl-6 order-1 order-xl-2 m--align-right">
+                                                            <div class="m-separator m-separator--dashed d-xl-none"></div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="supplier_invoice_datatable" id="scrolling_both"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <fieldset class="border p-2">
+                                    <legend class="w-auto">Debt Information</legend>
+                                    <div class="form-group m-form__group row">
+                                        <div class="col-sm-4 col-md-4 col-lg-4">
+                                            <label class="form-control-label">
+                                                Debt Total Amount
+                                            </label>
+    
+                                            @component('label::data-info')
+                                                @slot('text', 'generated')
+                                            @endcomponent
+                                        </div>
+                                        <div class="col-sm-4 col-md-4 col-lg-4">
+                                            <label class="form-control-label">
+                                                Payment Total Amount
+                                            </label>
+    
+                                            @component('label::data-info')
+                                                @slot('text', 'generated')
+                                            @endcomponent
+                                        </div>
+                                        <div class="col-sm-4 col-md-4 col-lg-4">
+                                            <label class="form-control-label">
+                                                Payment Total Amount
+                                            </label>
+    
+                                            @component('label::data-info')
+                                                @slot('text', 'generated')
+                                            @endcomponent
+                                        </div>
+                                    </div>
+                                </fieldset>
+                                {{-- datatables --}}
+                                <div class="form-group m-form__group row">
+                                    <div class="col-sm-12 col-md-12 col-lg-12">
+                                        <div class="m-portlet m-portlet--mobile">
+                                            <div class="m-portlet__body">
+                                                <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+                                                    <div class="row align-items-center">
+                                                        <div class="col-xl-6 order-2 order-xl-1">
+                                                            <div class="form-group m-form__group row align-items-center">
+                                                                <div class="col-md-6">
+                                                                    <div class="m-input-icon m-input-icon--left">
+                                                                        <input type="text" class="form-control m-input" placeholder="Search..."
+                                                                            id="generalSearch">
+                                                                        <span class="m-input-icon__icon m-input-icon__icon--left">
+                                                                            <span><i class="la la-search"></i></span>
+                                                                        </span>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-xl-6 order-1 order-xl-2 m--align-right">
+                                                            <div class="m-separator m-separator--dashed d-xl-none"></div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="adjustment_datatable" id="scrolling_both"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group m-form__group row ">
+                                    <div class="col-sm-12 col-md-12 col-lg-12 d-flex justify-content-end">
+                                        <div class="action-buttons">
+                                            @include('buttons::back')
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@push('footer-scripts')
+<script src="{{ asset('vendor/courier/frontend/account-payable/show.js')}}"></script>
+@endpush
