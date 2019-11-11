fc357dc67475f815bf1a5b7387cabd9032fa731e
Bagus Kurniawan <baguskurniawan999@gmail.com>
2019-11-11 12:14:33 +0700

    refactor Coa model to get coa from vendor

--- src/Model/Coa.php
+++ src/Model/Coa.php
@@ -2,13 +2,13 @@
 
 namespace Directoryxx\Finac\Model;
 
+use App\Models\Vendor;
+use App\Models\Customer;
 use Directoryxx\Finac\Model\MemfisModel;
 
 
 class Coa extends MemfisModel
 {
-
-
     protected $fillable = [
         'code',
         'name',
@@ -16,12 +16,40 @@ class Coa extends MemfisModel
         'description'
     ];
 
-	public function type()
-	{
-		return $this->belongsTo(
-			'App\Models\Type',
-			'type_id'
-		);
-	}
-	
+    /*************************************** RELATIONSHIP ****************************************/
+
+    /*
+     * Polymorphic: An entity can have zero or many coa.
+     *
+     * This function will get all of the owning coable models.
+     * See:
+     *
+     * @return mixed
+     */
+    public function customer()
+    {
+        return $this->morphedByMany(Customer::class, 'coable');
+    }
+
+    /*
+     * Polymorphic: An entity can have zero or many coa.
+     *
+     * This function will get all of the owning coable models.
+     * See:
+     *
+     * @return mixed
+     */
+    public function vendor()
+    {
+        return $this->morphedByMany(Vendor::class, 'coable');
+    }
+
+  public function type()
+  {
+    return $this->belongsTo(
+      'App\Models\Type',
+      'type_id'
+    );
+  }
+
 }
