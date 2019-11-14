ba2eb07a858b777e8b83974f3400ba3ef7388a26
Bagus Kurniawan <baguskurniawan999@gmail.com>
2019-11-14 13:21:19 +0700

    AP progres

--- src/assets/frontend/account-payable/create.js
+++ src/assets/frontend/account-payable/create.js
@@ -1,5 +1,8 @@
 let AccountPayable = {
-    init: function () {
+	init: function () {
+
+		let _url = window.location.origin;
+
 		let suuplier_invoice = $('.supplier_invoice_datatable').mDatatable({
 				data: {
 						type: 'remote',
@@ -209,7 +212,48 @@ let AccountPayable = {
 
 				]
 		});
-    }
+
+		let simpan = $('body').on('click', '#account_payable_save', function () {
+
+				let form = $(this).parents('form');
+				let _data = form.serialize();
+
+				$.ajax({
+						headers: {
+								'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+						},
+						type: 'post',
+						url: '/account-payable',
+						data: _data,
+						success: function (data) {
+								if (data.errors) {
+										if (data.errors.code) {
+												$('#code-error').html(data.errors.code[0]);
+
+
+												document.getElementById('code').value = code;
+												document.getElementById('name').value = name;
+												document.getElementById('type').value = type;
+												document.getElementById('level').value = level;
+												document.getElementById('description').value = description;
+												coa_reset();
+										}
+
+
+								} else {
+
+										toastr.success('Data berhasil disimpan.', 'Sukses', {
+												timeOut: 2000
+										});
+
+										setTimeout(function(){ 
+											location.href = `${_url}/account-payable/${data.uuid}/edit`; 
+										}, 2000);
+								}
+						}
+				});
+		});
+	}
 };
 
 jQuery(document).ready(function () {
