00658e370924941b96a9ac2e8a7e2f2fee5a3c3e
ibnuprtma <pibnu86@gmail.com>
2019-12-08 08:52:21 +0700

    fix js payroll

--- src/assets/frontend/payroll/process.js
+++ src/assets/frontend/payroll/process.js
@@ -102,6 +102,75 @@ let Payroll = {
 
             ]
         });
+
+        let payroll__information_table = $('.payroll_information_datatable').mDatatable({
+            data: {
+                    type: 'remote',
+                    source: {
+                            read: {
+                                    method: 'GET',
+                                    url: '',
+                                    map: function (raw) {
+                                            let dataSet = raw;
+
+                                            if (typeof raw.data !== 'undefined') {
+                                                    dataSet = raw.data;
+                                            }
+
+                                            return dataSet;
+                                    }
+                            }
+                    },
+                    pageSize: 10,
+                    serverPaging: !0,
+                    serverSorting: !0
+            },
+            layout: {
+                    theme: 'default',
+                    class: '',
+                    scroll: false,
+                    footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                    input: $('#generalSearch')
+            },
+            toolbar: {
+                    items: {
+                            pagination: {
+                                    pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                            }
+                    }
+            },
+            columns: [
+                {
+                    field: '#',
+                    title: 'No',
+                    width:'40',
+                    sortable: 'asc',
+                    filterable: !1,
+                    textAlign: 'center',
+                    template: (row, index, datatable) => {
+                        return (index + 1) + (datatable.getCurrentPage() - 1) * datatable.getPageSize()
+                    }
+                },
+                {
+                    field: '',
+                    title: 'Benefits Component',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+                {
+                    field: '',
+                    title: 'Total',
+                    sortable: 'asc',
+                    filterable: !1,
+                },
+
+            ]
+        });
     }
 };
 
