222d5172c90d9ea069ba6c433b0b63e4805ddaf7
ibnuprtma <pibnu86@gmail.com>
2019-12-18 16:30:30 +0700

    view bond

---
+++ src/assets/frontend/bond/index.js
@@ -0,0 +1,146 @@
+let Bond = {
+    init: function () {
+        $('.bond_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        method: 'GET',
+                        url: '',
+                        map: function (raw) {
+                            let dataSet = raw;
+
+                            if (typeof raw.data !== 'undefined') {
+                                dataSet = raw.data;
+                            }
+
+                            return dataSet;
+                        }
+                    }
+                },
+                pageSize: 10,
+                serverPaging: !1,
+                serverFiltering: !0,
+                serverSorting: !1
+            },
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: false,
+                footer: !1
+            },
+            sortable: !0,
+            filterable: !1,
+            pagination: !0,
+            search: {
+                input: $('#generalSearch')
+            },
+            toolbar: {
+                items: {
+                    pagination: {
+                        pageSizeSelect: [5, 10, 20, 30, 50, 100]
+                    }
+                }
+            },
+            columns: [
+                {
+                    field: '',
+                    title: 'Date',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 60
+                },
+                {
+                    field: '',
+                    title: 'Transaction No.',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: '',
+                    title: 'Person',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 60,
+                },
+                {
+                    field: '',
+                    title: 'Amount',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: '',
+                    title: 'Return Date',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: '',
+                    title: 'Paid Amount',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150,
+                },
+                {
+                    field: '',
+                    title: 'Description',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: '',
+                    title: 'Status',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: '',
+                    title: 'Created By',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: '',
+                    title: 'Approve by',
+                    sortable: 'asc',
+                    filterable: !1,
+                    width: 150
+                },
+                {
+                    field: 'Actions',
+                    width: 110,
+                    title: 'Actions',
+                    sortable: !1,
+                    overflow: 'visible',
+                    template: function (t, e, i) {
+											if (!t.approve) {
+												_html +=
+                          '<a href="'+_url+'/'+t.uuid+'/edit" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill edit" title="Edit" data-uuid=' +
+                          t.uuid +
+                          '>\t\t\t\t\t\t\t<i class="la la-pencil"></i>\t\t\t\t\t\t</a>\t\t\t\t\t\t' +
+                          '\t\t\t\t\t\t\t<a class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill  delete" href="#" data-uuid=' +
+                          t.uuid +
+                          ' title="Delete"><i class="la la-trash"></i> </a>\t\t\t\t\t\t\t' +
+                          '<a href="javascript:;" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill approve" title="Approve" data-uuid="' + t.uuid + '">' +
+                          '<i class="la la-check"></i>' +
+                          '</a>';
+											}
+
+                      return (_html);
+                    }
+                }
+            ]
+        });
+    }
+};
+
+jQuery(document).ready(function () {
+    Bond.init();
+});
---
+++ src/views/bond/edit.blade.php
@@ -0,0 +1,226 @@
+@extends('frontend.master')
+
+@section('content')
+<div class="m-subheader hidden">
+    <div class="d-flex align-items-center">
+        <div class="mr-auto">
+            <h3 class="m-subheader__title m-subheader__title--separator">
+                Bond
+            </h3>
+            <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
+                <li class="m-nav__item m-nav__item--home">
+                    <a href="" class="m-nav__link m-nav__link--icon">
+                        <i class="m-nav__link-icon la la-home"></i>
+                    </a>
+                </li>
+                <li class="m-nav__separator">
+                    -
+                </li>
+                <li class="m-nav__item">
+                    <a href="#" class="m-nav__link">
+                        <span class="m-nav__link-text">
+                            Bond
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+@include('cashbookview::coamodal')
+<div class="m-content">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="m-portlet">
+                <div class="m-portlet__head">
+                    <div class="m-portlet__head-caption">
+                        <div class="m-portlet__head-title">
+                            <span class="m-portlet__head-icon m--hide">
+                                <i class="la la-gear"></i>
+                            </span>
+
+                            @include('label::edit')
+
+                            <h3 class="m-portlet__head-text">
+                                Bond
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="m-portlet m-portlet--mobile">
+                    <div class="m-portlet__body">
+                        <form id="MasterAssetForm">
+                            <div class="m-portlet__body">
+                                <div class="form-group m-form__group row ">
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Person @include('label::required')
+                                        </label>
+
+                                        @component('input::select')
+                                            @slot('id', 'employee')
+                                            @slot('text', 'Person')
+                                            @slot('name', 'employee')
+                                            @slot('id_error', 'employee')
+                                        @endcomponent
+                                    </div>
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Date @include('label::required')
+                                        </label>
+
+                                        @component('input::datepicker')
+                                            @slot('id', 'date')
+                                            @slot('name', 'date')
+                                            @slot('text', 'Date')
+                                        @endcomponent
+                                    </div>
+                                </div>
+                                <div class="form-group m-form__group row ">
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Date Returned @include('label::required')
+                                        </label>
+
+                                        @component('input::datepicker')
+                                            @slot('id', 'date-required')
+                                            @slot('text', 'Date Returned')
+                                            @slot('name', 'Date Required')
+                                            @slot('id_error', 'date-required')
+                                        @endcomponent
+                                    </div>
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Amount @include('label::required')
+                                        </label>
+
+                                        @component('input::number')
+                                            @slot('id', 'amount')
+                                            @slot('name', 'amount')
+                                            @slot('text', 'Amount')
+                                        @endcomponent
+                                    </div>
+                                </div>
+                                <div class="form-group m-form__group row ">
+                                    <div class="col-sm-12 col-md-12 col-lg-12">
+                                        <h4>Cash/Bank Account @include('label::required')</h4>
+                                    </div>
+                                </div>
+                                <div class="form-group m-form__group row ">
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Account Code
+                                        </label>
+
+
+                                        @component('input::inputrightbutton')
+                                            @slot('id', 'coa')
+                                            @slot('text', 'coa')
+                                            @slot('name', 'coa')
+                                            @slot('type', 'text')
+                                            @slot('style', 'width:100%')
+                                            @slot('help_text','Account Code')
+                                            @slot('data_target', '#coa_modal')
+                                        @endcomponent
+                                    </div>
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Account Name
+                                        </label>
+
+                                        @component('input::input')
+                                            @slot('id', 'account_name_1')
+                                            @slot('name', 'account_name_1')
+                                            @slot('text', 'Account Name')
+                                        @endcomponent
+                                    </div>
+                                </div>
+                                <div class="form-group m-form__group row ">
+                                    <div class="col-sm-12 col-md-12 col-lg-12">
+                                        <h4>Bond Account @include('label::required')</h4>
+                                    </div>
+                                </div>
+                                <div class="form-group m-form__group row ">
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Account Code
+                                        </label>
+
+                                        @component('input::inputrightbutton')
+                                            @slot('id', 'coa_bond')
+                                            @slot('text', 'coa')
+                                            @slot('name', 'coa')
+                                            @slot('type', 'text')
+                                            @slot('style', 'width:100%')
+                                            @slot('help_text','Account Code')
+                                            @slot('data_target', '#coa_modal')
+                                        @endcomponent
+                                    </div>
+                                    <div class="col-sm-6 col-md-6 col-lg-6">
+                                        <label class="form-control-label">
+                                            Account Name
+                                        </label>
+
+                                        @component('input::input')
+                                            @slot('id', 'account_name_2')
+                                            @slot('name', 'account_name_2')
+                                            @slot('text', 'Account Name')
+                                        @endcomponent
+                                    </div>
+                                </div>
+                                <div class="form-group m-form__group row ">
+                                    <div class="col-sm-12 col-md-12 col-lg-12">
+                                        <label class="form-control-label">
+                                            Description
+                                        </label>
+
+                                        @component('input::textarea')
+                                            @slot('id', 'description')
+                                            @slot('text', 'Description')
+                                            @slot('name', 'description')
+                                            @slot('rows','5')
+                                        @endcomponent
+                                    </div>
+                                </div>
+                                <div class="form-group m-form__group row ">
+                                    <div class="col-sm-12 col-md-12 col-lg-12 d-flex justify-content-end">
+                                        <div class="action-buttons">
+                                            @component('buttons::submit')
+                                                @slot('type', 'button')
+                                                @slot('id','bond_save')
+                                            @endcomponent
+
+                                            @include('buttons::reset')
+
+                                            @include('buttons::back')
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('footer-scripts')
+<script src="{{ asset('vendor/courier/frontend/functions/reset.js')}}"></script>
+
+
+<script src="{{ asset('vendor/courier/frontend/functions/fill-combobox/coa.js')}}"></script>
+
+<script src="{{ asset('vendor/courier/frontend/coamodal.js')}}"></script>
+
+<script src="{{ asset('vendor/courier/frontend/functions/select2/employee.js')}}"></script>
+<script src="{{ asset('vendor/courier/frontend/functions/fill-combobox/employee.js')}}"></script>
+
+<script src="{{ asset('vendor/courier/frontend/functions/datepicker/date.js')}}"></script>
+<script src="{{ asset('vendor/courier/frontend/functions/datepicker/date-required.js')}}"></script>
+
+<script src="{{ asset('vendor/courier/frontend/bond/edit.js')}}"></script>
+
+<script src="{{ asset('vendor/courier/vendors/custom/datatables/datatables.bundle.js')}}"></script>
+@endpush
