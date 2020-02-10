40cb0da11c050d09f92db6a6dd5d25aa15c22368
ibnuprtma <pibnu86@gmail.com>
2020-02-10 11:18:06 +0700

    fix typo

--- src/views/ar-report/account-receivables/modal.blade.php
+++ src/views/ar-report/account-receivables/modal.blade.php
@@ -2,7 +2,7 @@
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="TitleModaladjustment">Account Receivables History</h5>
+                    <h5 class="modal-title" id="TitleModaladjustment">Report Filter</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
