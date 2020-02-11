5880603173bc30a1cea15b433bd55ca284e09535
Bagus Kurniawan <baguskurniawan999@gmail.com>
2020-02-11 11:30:48 +0700

    make cashbook_ref nullable

---
+++ src/migrations/2020_10_27_154824_make_cashbook_ref_nullable_cashbook_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeCashbookRefNullableCashbookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		Schema::table('cashbooks', function (Blueprint $table) {
+	        $table->dropColumn('cashbook_ref');
+	        $table->text('cashbook_ref')->nullable();
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
