760dab24f72c75bd490b3ae13d05724203817f87
ibnuprtma <pibnu86@gmail.com>
2020-06-20 16:40:21 +0700

    wip

---
+++ src/views/invoice/invoice-workshop/account-profit-center/index.blade.php
@@ -0,0 +1,71 @@
+<div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            MMF - IERA Shop
+        </label>
+
+        <select class="_select2 form-control" autocomplete="true" name="sales" style="width:100%">
+            <option value=""></option>
+        </select>
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            MMF - Mechanical Shop
+        </label>
+
+        <select class="_select2 form-control" autocomplete="true" name="vat" style="width:100%">
+            <option value=""></option>
+        </select>
+    </div>
+</div>
+<div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            MMF - Propulsion and Accessories Shop
+        </label>
+
+        <select class="_select2 form-control" autocomplete="true" name="othercost" style="width:100%">
+            <option value=""></option>
+        </select>
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            MMF - Logistic
+        </label>
+
+        <select class="_select2 form-control" autocomplete="true" name="othercost" style="width:100%">
+            <option value=""></option>
+        </select>
+    </div>
+</div>
+<div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            MMF - Discount
+        </label>
+
+        <select class="_select2 form-control" autocomplete="true" name="othercost" style="width:100%">
+            <option value=""></option>
+        </select>
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            MMF - Other Revenue
+        </label>
+
+        <select class="_select2 form-control" autocomplete="true" name="othercost" style="width:100%">
+            <option value=""></option>
+        </select>
+    </div>
+</div>
+<div class="form-group m-form__group row ">
+    <div class="col-sm-6 col-md-6 col-lg-6">
+        <label class="form-control-label">
+            VAT
+        </label>
+
+        <select class="_select2 form-control" autocomplete="true" name="othercost" style="width:100%">
+            <option value=""></option>
+        </select>
+    </div>
+</div>
