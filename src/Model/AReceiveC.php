4214dfb7829ff1c24727a929ee3eed4efe4fe8bb
Bagus Kurniawan <baguskurniawan999@gmail.com>
2020-07-09 17:11:43 +0700

    AR > AReceiveC > add relation to ara and display gap

--- src/Model/AReceiveC.php
+++ src/Model/AReceiveC.php
@@ -10,13 +10,28 @@ class AReceiveC extends MemfisModel
     protected $table = "a_receive_c";
 
 	protected $fillable = [
+	    'ara_id',
 	    'uuid',
 	    'transactionnumber',
 	    'id_invoice',
 	    'code',
-	    'difference',
+	    'crdit',
+	    'debit',
 	    'description',
-	];
+    ];
+    
+    protected $appends = [
+        'gap'
+    ];
+
+    public function getGapAttribute()
+    {
+        if ($this->credit != 0) {
+            return $this->credit;
+        }else{
+            return $this->debit;
+        }
+    }
 
 	public function ar()
 	{
@@ -25,7 +40,12 @@ class AReceiveC extends MemfisModel
 			'transactionnumber',
 			'transactionnumber'
 		);
-	}
+    }
+    
+    public function ara()
+    {
+        return $this->belongsTo(AReceiveA::class, 'ara_id');
+    }
 
 	public function coa()
 	{
