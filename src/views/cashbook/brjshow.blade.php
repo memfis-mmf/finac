d1947eedee867ac01e25923cdd200fd6fc1b7598
Bagus Kurniawan <baguskurniawan999@gmail.com>
2020-07-13 10:55:52 +0700

    FA > cashbook > set active menu

--- src/views/cashbook/brjshow.blade.php
+++ src/views/cashbook/brjshow.blade.php
@@ -1,5 +1,6 @@
 @extends('frontend.master')
 
+@section('faCashbook', 'm-menu__item--active')
 @section('content')
 <div class="m-subheader hidden">
     <div class="d-flex align-items-center">
@@ -267,6 +268,15 @@
 @endsection
 
 @push('footer-scripts')
+    <script>
+        $(document).ready(function() {
+            let currentUrl = window.location.href;
+            let _hash = currentUrl.split('#');
+            if (_hash.length < 2) {
+                window.location.href=currentUrl+"#faCashbook";
+            }
+        });
+    </script>
 <script src="{{ asset('vendor/courier/frontend/functions/reset.js')}}"></script>
 <script src="{{ asset('vendor/courier/frontend/functions/fill-combobox/coa.js')}}"></script>
 <script src="{{ asset('vendor/courier/frontend/functions/datepicker/date.js')}}"></script>
