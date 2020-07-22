52645ac425e69111843789d856891f82671a99ed
Bagus Kurniawan <baguskurniawan999@gmail.com>
2020-07-22 23:59:11 +0700

    AR > edit > modal invoice edit fix text

--- src/views/account-receivable/modal-edit-invoice.blade.php
+++ src/views/account-receivable/modal-edit-invoice.blade.php
@@ -2,7 +2,7 @@
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="TitleModalSupplierInvoice">Supplier Invoice - Amount to Pay</h5>
+        <h5 class="modal-title" id="TitleModalSupplierInvoice">Invoice - Amount Receive</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -74,7 +74,7 @@
             <div class="form-group m-form__group row ">
               <div class="col-sm-6 col-md-6 col-lg-6">
                 <label class="form-control-label">
-                  Amount to Pay
+                  Amount Receive
                 </label>
 
                 <div class="input-group">
