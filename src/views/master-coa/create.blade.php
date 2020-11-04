356638334698f5141e9e90e56f5ac1b6f1e98ff0
Bagus Kurniawan <baguskurniawan999@gmail.com>
2020-11-04 15:59:41 +0700

    master coa > creat > add asteric to sub account

--- src/views/master-coa/create.blade.php
+++ src/views/master-coa/create.blade.php
@@ -81,7 +81,7 @@
                                 <div class="form-group m-form__group row ">
                                     <div class="col-sm-6 col-md-6 col-lg-6">
                                         <label class="form-control-label">
-                                            Sub Account
+                                            Sub Account @include('label::required')
                                         </label>
 
                                         @component('input::select2')
