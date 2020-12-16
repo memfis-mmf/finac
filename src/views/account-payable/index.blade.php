994600b9ed5e7d105ffe6950eaa5c7c6fce99736
Yendy Rachmatulloh <yendy_rachmatulloh@yahoo.com>
2020-12-16 12:34:09 +0700

    Typo & UI Fixing

--- src/views/account-payable/index.blade.php
+++ src/views/account-payable/index.blade.php
@@ -20,33 +20,6 @@
   }
 </style>
 
-<div class="m-subheader hidden">
-    <div class="d-flex align-items-center">
-        <div class="mr-auto">
-            <h3 class="m-subheader__title m-subheader__title--separator">
-                Account Payable
-            </h3>
-            <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
-                <li class="m-nav__item m-nav__item--home">
-                    <a href="" class="m-nav__link m-nav__link--icon">
-                        <i class="m-nav__link-icon la la-home"></i>
-                    </a>
-                </li>
-                <li class="m-nav__separator">
-                    -
-                </li>
-                <li class="m-nav__item">
-                    <a href="#" class="m-nav__link">
-                        <span class="m-nav__link-text">
-                            Account Payable
-                        </span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div>
-
 <div class="m-content">
     <div class="row">
         <div class="col-lg-12">
@@ -97,25 +70,29 @@
                             @include('accountpayableview::filter')
                         </div> --}}
                         {{-- <div class="account_payable_datatable" id="scrolling_both"></div> --}}
-                        <table class="table table-striped table-bordered table-hover table-checkable account_payable_datatable">
-                            <thead>
-                                <tr>
-                                    <th>Date</th>
-                                    <th>AP NO</th>
-                                    <th>Supplier Name</th>
-                                    <th>Ref No</th>
-                                    <th>Currency</th>
-                                    <th>Exchange Rate</th>
-                                    <th>Total Transaction</th>
-                                    <th>Account Code</th>
-                                    <th>Description</th>
-                                    <th>Status</th>
-                                    <th>Created By</th>
-                                    <th>Approved By</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                        </table>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <table class="table table-striped table-bordered table-hover table-checkable account_payable_datatable">
+                                    <thead>
+                                        <tr>
+                                            <th>Date</th>
+                                            <th>AP NO</th>
+                                            <th>Supplier Name</th>
+                                            <th>Ref No</th>
+                                            <th>Currency</th>
+                                            <th>Exchange Rate</th>
+                                            <th>Total Transaction</th>
+                                            <th>Account Code</th>
+                                            <th>Description</th>
+                                            <th>Status</th>
+                                            <th>Created By</th>
+                                            <th>Approved By</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
