d47b9ced868616c56095e3e6643f5c55571a82a2
Yemimatifani <yemimatifani@gmail.com>
2021-01-08 10:51:37 +0700

    Good Recieved -> Goods Received Notes (GRN)

--- src/views/supplier-invoice/grn/modal-create.blade.php
+++ src/views/supplier-invoice/grn/modal-create.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="TitleModalproject">Good Recieved Notes (GRN)</h5>
+                <h5 class="modal-title" id="TitleModalproject">Goods Received Notes (GRN)</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
