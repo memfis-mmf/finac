d9658c69fcd4773fe7ac0bf827c2ce6e7d608d2e
Bagus Kurniawan <baguskurniawan999@gmail.com>
2021-01-11 10:37:10 +0700

    cashbook > show > handle page show in edit.js

--- src/assets/frontend/cashbook/edit.js
+++ src/assets/frontend/cashbook/edit.js
@@ -1,6 +1,13 @@
 let Coa = {
   init: function () {
 
+    if (page_type == 'show') {
+      $('input').attr('disabled', 'disabled');
+      $('select').attr('disabled', 'disabled');
+      $('textarea').attr('disabled', 'disabled');
+      $('button').attr('disabled', 'disabled');
+    }
+
 		let _url = window.location.origin;
 		let cashbook_uuid = $('input[name=cashbook_uuid]').val();
 		let number_format = new Intl.NumberFormat('de-DE');
@@ -151,6 +158,10 @@ let Coa = {
                     '\t\t\t\t\t\t\t<a class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill  delete" href="#" data-uuid=' +
                     t.uuid +
                     ' title="Delete"><i class="la la-trash"></i> </a>\t\t\t\t\t\t\t'
+
+                  if (page_type == 'show') {
+                    _html = '';
+                  }
 
                   return _html;
                 }
