134537a252fb72be2039068a1b22139281d64c75
M.Husni Thamrin <husni.sosnet@gmail.com>
2021-01-25 14:09:12 +0700

    upload manual book

--- src/views/journal/index.blade.php
+++ src/views/journal/index.blade.php
@@ -27,7 +27,7 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="m-portlet">
-                <div class="m-portlet__head">
+                <div class="m-portlet__head ribbon ribbon-top ribbon-ver">
                     <div class="m-portlet__head-caption">
                         <div class="m-portlet__head-title">
                             <span class="m-portlet__head-icon m--hide">
@@ -41,6 +41,9 @@
                             </h3>
                         </div>
                     </div>
+                    @component('frontend.common.buttons.read-help')
+                        @slot('href', '/journal.pdf/help')
+                    @endcomponent
                 </div>
                 <div class="m-portlet m-portlet--mobile">
                     <div class="m-portlet__body">
--- src/views/supplier-invoice/index.blade.php
+++ src/views/supplier-invoice/index.blade.php
@@ -35,7 +35,7 @@
   <div class="row">
     <div class="col-lg-12">
       <div class="m-portlet">
-        <div class="m-portlet__head">
+        <div class="m-portlet__head ribbon ribbon-top ribbon-ver">
           <div class="m-portlet__head-caption">
             <div class="m-portlet__head-title">
               <span class="m-portlet__head-icon m--hide">
@@ -49,6 +49,9 @@
               </h3>
             </div>
           </div>
+          @component('frontend.common.buttons.read-help')
+              @slot('href', '/supplier-invoice.pdf/help')
+          @endcomponent
         </div>
         <div class="m-portlet m-portlet--mobile">
           <div class="m-portlet__body">
