e22d768e284637839955243385809ae9c78e1624
M. Husni Thamrin <husni.sosnet@gmail.com>
2021-02-24 16:23:48 +0700

    upload manual book 24 Feb 2021

--- src/views/account-payable/index.blade.php
+++ src/views/account-payable/index.blade.php
@@ -24,7 +24,7 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="m-portlet">
-                <div class="m-portlet__head">
+                <div class="m-portlet__head ribbon ribbon-top ribbon-ver">
                     <div class="m-portlet__head-caption">
                         <div class="m-portlet__head-title">
                             <span class="m-portlet__head-icon m--hide">
@@ -38,6 +38,9 @@
                             </h3>
                         </div>
                     </div>
+                    @component('frontend.common.buttons.read-help')
+                        @slot('href', '/account-payable-payment.pdf/help')
+                    @endcomponent
                 </div>
                 <div class="m-portlet m-portlet--mobile">
                     <div class="m-portlet__body">
