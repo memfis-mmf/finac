1dfc7955d2d9f1e562be5fc029a0b7e568dff2a5
Bagus Kurniawan <baguskurniawan999@gmail.com>
2021-03-02 10:00:26 +0700

    outstanding AR, outstanding AP > modal, hide department

--- src/views/ar-report/outstanding/modal-ap.blade.php
+++ src/views/ar-report/outstanding/modal-ap.blade.php
@@ -33,7 +33,7 @@
                             </div>
                         </div>
                         <div class="form-group m-form__group row">
-                            <div class="col-sm-6 col-md-6 col-lg-6">
+                            {{-- <div class="col-sm-6 col-md-6 col-lg-6">
                                 <label class="form-control-label">
                                     Department
                                 </label>
@@ -44,7 +44,7 @@
                                     @slot('class', 'department')
                                     @slot('id_error', 'department_id')
                                 @endcomponent
-                            </div>
+                            </div> --}}
                             <div class="col-sm-6 col-md-6 col-lg-6">
                                 <label class="form-control-label">
                                     Location
