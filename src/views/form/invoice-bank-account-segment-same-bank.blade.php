584eac8239d1789c5a1e0db533ea6be0efeec2b9
Bagus Kurniawan <baguskurniawan999@gmail.com>
2021-03-18 10:43:10 +0700

    invoice > print > fix print layout

--- src/views/form/invoice-bank-account-segment-same-bank.blade.php
+++ src/views/form/invoice-bank-account-segment-same-bank.blade.php
@@ -31,26 +31,41 @@
         <td valign="top" width="31%">{{@$bank3->number}}</td>
     </tr>
     <tr>
-        <td valign="top" width="18%">Swift Code</td>
-        <td valign="top" width="1%">:</td>
-        <td valign="top" width="31%">{{$bank1->swift_code}}</td>
+        @if ($bank1->currency->code != 'idr')
+          <td valign="top" width="18%">Swift Code</td>
+          <td valign="top" width="1%">:</td>
+          <td valign="top" width="31%">{{$bank1->swift_code}}</td>
+        @else 
+          <td valign="top" width="18%"></td>
+          <td valign="top" width="1%"></td>
+          <td valign="top" width="31%"></td>
+        @endif
         {{-- bank 2 --}}
-        <td valign="top" width="18%">Swift Code</td>
-        <td valign="top" width="1%">:</td>
-        <td valign="top" width="31%">{{@$bank3->swift_code}}</td>
+        @if (@$bank3->currency->code != 'idr')
+          <td valign="top" width="18%">Swift Code</td>
+          <td valign="top" width="1%">:</td>
+          <td valign="top" width="31%">{{@$bank3->swift_code}}</td>
+        @else
+          <td valign="top" width="18%"></td>
+          <td valign="top" width="1%"></td>
+          <td valign="top" width="31%"></td>
+        @endif
     </tr>
 </table>
 <table width="100%" cellpadding="4" style="padding-top:10px" style="margin-top:-5px">
     <tr>
-        <td valign="top" width="18%">Bank Acc <b>{{ strtoupper($bank1->currency->code) }}</b> No.</td>
+        <td valign="top" width="18%">Bank Acc <b>{{ strtoupper(@$bank2->currency->code) }}</b> No.</td>
         <td valign="top" width="1%">:</td>
         <td valign="top" width="">{{@$bank2->number}}</td>
     </tr>
-    <tr>
-        <td valign="top" width="18%">Swift Code</td>
-        <td valign="top" width="1%">:</td>
-        <td valign="top" width="">{{@$bank2->swift_code}}</td>
-    </tr>
+    @if (@$bank2->currency->code != 'idr')
+      <tr>
+          <td valign="top" width="18%">Swift Code</td>
+          <td valign="top" width="1%">:</td>
+          <td valign="top" width="">{{@$bank2->swift_code}}</td>
+      </tr>
+    @endif
+    
     <tr>
         <td colspan="3" align="center"><i>PAYMENT SHOULD BE RECEIVED IN FULL AMOUNT</i></td>
     </tr>
