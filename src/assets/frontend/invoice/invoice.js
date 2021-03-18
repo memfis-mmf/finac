598bdf822d00120f27dc0dbd5af24977486f0a89
Bagus Kurniawan <baguskurniawan999@gmail.com>
2021-03-18 10:34:11 +0700

    invoice > index > fix search transaction number

--- src/assets/frontend/invoice/invoice.js
+++ src/assets/frontend/invoice/invoice.js
@@ -25,7 +25,7 @@ let Invoice = {
           columns: [
             {data: 'created_at', searchable: false, visible: false},
             {data: 'date', name: 'transactiondate'},
-            {data: 'transaction_number_link'},
+            {data: 'transaction_number_link', name: 'transactionnumber'},
             {data: 'xstatus', searchable: false, orderable: false},
             {data: 'customer.name'},
             {data: 'quotations.number', defaultContent: '-', render: (data, type, row) => {
