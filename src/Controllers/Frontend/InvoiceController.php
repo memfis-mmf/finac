bf72591f581275f016eb188783cfbbb6f9f5835e
Bagus Kurniawan <baguskurniawan999@gmail.com>
2021-03-23 12:05:27 +0700

    invoice > print > fix logic same bank

--- src/Controllers/Frontend/InvoiceController.php
+++ src/Controllers/Frontend/InvoiceController.php
@@ -1446,69 +1446,49 @@ class InvoiceController extends Controller
         $bank_account_2 = $invoice->bank2;
         $bank_account_3 = $invoice->bank3;
 
-        $bank_id = [
-            $bank_account_1->bank->id,
-            @$bank_account_2->bank->id ?? null,
-            @$bank_account_3->bank->id ?? null,
+        $bank_account = [
+            $bank_account_1,
+            $bank_account_2 ?? null,
+            $bank_account_3 ?? null,
         ];
 
-        $bank_account_name = [
-            $bank_account_1->name,
-            @$bank_account_2->name ?? null,
-            @$bank_account_3->name ?? null,
-        ];
+        $bank_account = array_filter($bank_account);
+        $bank_account = array_values($bank_account);
 
-        $bank_id = array_filter($bank_id);
-        $bank_id = array_values($bank_id);
+        $data_same_bank = [];
 
-        $bank_account_name = array_filter($bank_account_name);
-        $bank_account_name = array_values($bank_account_name);
+        for ($index=0; $index < count($bank_account); $index++) { 
+            for ($index_2=1; $index_2 < count($bank_account); $index_2++) { 
+                if ($bank_account[$index]->bank->id == $bank_account[$index_2]->bank->id) {
+                    $data_same_bank = [
+                        'bank1' => $bank_account[$index],
+                        'bank2' => $bank_account[$index_2],
+                    ];
 
-        $total_bank_id_difference = count(array_unique($bank_id));
-        $total_bank_name_difference = count(array_unique($bank_account_name));
+                    unset($bank_account[$index]);
+                    unset($bank_account[$index_2]);
+                    $bank_account = array_values($bank_account);
 
-        if ($total_bank_id_difference < 3 and $total_bank_name_difference < 3) {
+                    $data_same_bank['bank3'] = $bank_account[0] ?? null;
 
-            if ($bank_id[0] == $bank_id[1] and $bank_account_name[0] == $bank_account_name[1]) {
-                $data = [
-                    'bank1' => $bank_account_1,
-                    'bank2' => $bank_account_2,
-                    'bank3' => $bank_account_3,
-                ];
+                    break;
+                }
             }
 
-            if ($bank_id[0] == ($bank_id[2] ?? null) and $bank_account_name[0] == ($bank_account_name[2] ?? null)) {
-                $data = [
-                    'bank1' => $bank_account_1,
-                    'bank2' => $bank_account_3,
-                    'bank3' => $bank_account_2,
-                ];
+            if (count($data_same_bank) > 1) {
+                break;
             }
-
-            if (($bank_id[1] ?? null) == ($bank_id[2] ?? null) and ($bank_account_name[1] ?? null) == ($bank_account_name[2] ?? null)) {
-                $data = [
-                    'bank1' => $bank_account_2,
-                    'bank2' => $bank_account_3,
-                    'bank3' => $bank_account_1,
-                ];
-
-            }
-
-            return view('formview::invoice-bank-account-segment-same-bank', $data);
-
         }
 
-        if (
-            $total_bank_id_difference == 3 
-            or ($total_bank_id_difference == 2 and count($bank_account_name) == 3)
-            or count($bank_account_name) == 1
-            or count($bank_account_name) == 2
-        ) {
+        if (count($data_same_bank) > 1) {
+            return view('formview::invoice-bank-account-segment-same-bank', $data_same_bank);
+        }
+
+        if (count($data_same_bank) < 1) {
             $data = [
                 'bank1' => $bank_account_1,
-                'bank2' => $bank_account_2,
+                'bank2' => $bank_account_2 ?? $bank_account_3,
             ];
-
             return view('formview::invoice-bank-account-segment', $data);
         }
     }
