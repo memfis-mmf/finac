4cec529b16ee0810d83d6c0e29f2ee0b891ef1dc
Bagus Kurniawan <baguskurniawan999@gmail.com>
2021-04-15 14:13:44 +0700

    model > memfis model > create generate number

--- src/Model/MemfisModel.php
+++ src/Model/MemfisModel.php
@@ -1,5 +1,7 @@
 <?php
 namespace memfisfa\Finac\Model;
+
+use Carbon\Carbon;
 use memfisfa\Finac\Traits\UuidKey;
 use memfisfa\Finac\Traits\Timestampable;
 use Illuminate\Database\Eloquent\Model;
@@ -20,5 +22,28 @@ class MemfisModel extends Model implements Auditable
     public function getRouteKeyName()
     {
         return 'uuid';
+    }
+
+    protected static function generateTransactionNumber($class, $column, $code)
+    {
+		$data = $class::withTrashed()
+            ->whereYear('created_at', Carbon::now()->format('Y'))
+			->where($column, 'like', $code.'%')
+            ->orderBy($column, 'desc')
+            ->first();
+
+        if (! $data) {
+            $order = 1;
+        } else {
+            $explode = explode('/', $data->$column);
+            $end = end($explode);
+            $order = ltrim($end, '0') + 1;
+        }
+
+		$number = str_pad($order, 5, '0', STR_PAD_LEFT);
+
+		$code = $code."-".date('Y')."/".$number;
+
+		return $code;
     }
 }
