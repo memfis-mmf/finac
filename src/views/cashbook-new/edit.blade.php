30229942e65d86cd9258ec957c549bf40bb34710
Rachmatulloh Yendy <38489513+yendyr@users.noreply.github.com>
2022-01-24 15:42:47 +0700

    Fix Typo

--- src/views/cashbook-new/edit.blade.php
+++ src/views/cashbook-new/edit.blade.php
@@ -547,6 +547,7 @@
                                 @endcomponent
                             </div>
                         </div>
+                        
                         <div class="form-group m-form__group row ">
                             <div class="col-sm-12 col-md-12 col-lg-12">
                                 <label class="form-control-label">
