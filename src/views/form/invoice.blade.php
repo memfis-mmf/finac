913b60395bc68ea92e0b71cd03a8cf92c02377b1
Yemima Tifani <66351352+Yemimatifani@users.noreply.github.com>
2022-01-31 13:56:19 +0700

    revisi 4 -_-

--- src/views/form/invoice.blade.php
+++ src/views/form/invoice.blade.php
@@ -561,7 +561,7 @@
                                 <td width="100%" valign="top" align="center">{{$invoice->location.', '.date('M d, Y', strtotime($invoice->transactiondate))}}</td>
                             </tr>
                             <tr>
-                                <td width="100%" height="90"></td>
+                                <td width="100%" height="87"></td>
                             </tr>
                             <tr>
                                 <td width="100%" valign="top" align="center"><b>{{$invoice->presdir}}</b></td>
