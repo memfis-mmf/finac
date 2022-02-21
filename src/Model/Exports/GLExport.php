2fc15bd5d546d73b209f590538298ead4fcc6c4b
Yemima Tifani <66351352+Yemimatifani@users.noreply.github.com>
2022-02-21 19:27:41 +0700

    test numbering format GL export

--- src/Model/Exports/GLExport.php
+++ src/Model/Exports/GLExport.php
@@ -16,6 +16,18 @@ class GLExport implements FromView, ShouldAutoSize
         $this->data = $data;
     }
 
+    public function columnFormats(): array
+    {
+        return [
+            'I' => NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+            'J' => NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+            'K' => NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+            'L' => NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+            'M' => NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+            'N' => NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+        ];
+    }
+
     public function view(): View
     {
         return view('generalledgerview::export', $this->data);
