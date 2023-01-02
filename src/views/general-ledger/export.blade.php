ead9f2e0ef0860a4828c5ca76236bd1efac4afcb
Bagus Kurnaiwan <baguskurniawan999@gmail.com>
2023-01-02 09:34:43 +0700

    GL > export > skip empty value

--- src/views/general-ledger/export.blade.php
+++ src/views/general-ledger/export.blade.php
@@ -5,87 +5,92 @@
       <td> {{$items['data'][0]->AccountCode}} - <span> {{$items['data'][0]->Name}}</span> </td>
     </tr> --}}
     <tr>
-      <td>Period </td>
-      <td>:</td>
-      <td>{{$beginDate.' - '.$endingDate}}</td>
+        <td>Period </td>
+        <td>:</td>
+        <td>{{ $beginDate . ' - ' . $endingDate }}</td>
     </tr>
 </table>
 
 <table class="table table-striped table-bordered table-hover">
-  <thead>
-    <tr>
-      <th>No</th>
-      <td>Account Code</td>
-      <td>Account Name</td>
-      <th>Date</th>
-      <th>Transaction No.</th>
-      <th>Ref. No.</th>
-      <th>Description</th>
-      <th>Currency</th>
-      <th>Foreign Total</th>
-      <th>Rate</th>
-      <th>Debit</th>
-      <th>Credit</th>
-      <th>Ending Balance</th>
-      <th>Project Number</th>
-      <th>Purchase Order Number</th>
-    </tr>
-  </thead>
-  <tbody>
-    @php
-        $number = 0
-    @endphp
-    @foreach ($data as $items)
-        @if (! isset($items['data'][0]->AccountCode))
+    <thead>
+        <tr>
+            <th>No</th>
+            <td>Account Code</td>
+            <td>Account Name</td>
+            <th>Date</th>
+            <th>Transaction No.</th>
+            <th>Ref. No.</th>
+            <th>Description</th>
+            <th>Currency</th>
+            <th>Foreign Total</th>
+            <th>Rate</th>
+            <th>Debit</th>
+            <th>Credit</th>
+            <th>Ending Balance</th>
+            <th>Project Number</th>
+            <th>Purchase Order Number</th>
+        </tr>
+    </thead>
+    <tbody>
         @php
-            continue;
+            $number = 0;
         @endphp
-        @endif
-        @foreach ($items['data'] as $index => $item)
-        @php
-            $number++;
-            $total_foreign += (($item->Debit != 0)? $item->Debit: $item->Credit) / $item->rate;
-            $total_debit += $item->Debit;
-            $total_credit += $item->Credit;
-            $total_ending_balance += $item->endingBalance;
-        @endphp
+        @foreach ($data as $items)
+            @if (!isset($items['data'][0]->AccountCode))
+                @php
+                    continue;
+                @endphp
+            @endif
+            @foreach ($items['data'] as $index => $item)
+                @if ($item->Debit == 0 and $item->Credit == 0)
+                    @php
+                        continue;
+                    @endphp
+                @endif
+                @php
+                    $number++;
+                    $total_foreign += ($item->Debit != 0 ? $item->Debit : $item->Credit) / $item->rate;
+                    $total_debit += $item->Debit;
+                    $total_credit += $item->Credit;
+                    $total_ending_balance += $item->endingBalance;
+                @endphp
+                <tr>
+                    <td>{{ $number }}</td>
+                    <td>{{ $items['data'][0]->AccountCode }}</td>
+                    <td>{{ $items['data'][0]->Name }}</td>
+                    <td>{{ $carbon::parse($item->TransactionDate)->format('d/m/Y') }}</td>
+                    <td>{!! $item->voucher_linked !!}</td>
+                    <td>{{ $item->RefNo }}</td>
+                    <td>{{ $item->Description }}</td>
+                    <td>{{ strtoupper($item->currency->code) }}</td>
+                    <td>
+                        {{ ($item->Debit != 0 ? $item->Debit : $item->Credit) / $item->rate, 2 }}
+                    </td>
+                    <td>{{ $item->rate }}</td>
+                    <td>{{ $item->Debit }}</td>
+                    <td>{{ $item->Credit * -1 }}</td>
+                    <td>{{ $item->endingBalance }}</td>
+                    <td>{{ $item->project_number }}</td>
+                    <td>{{ $item->po_number }}</td>
+                </tr>
+            @endforeach
+        @endforeach
         <tr>
             <td>{{ $number }}</td>
-            <td>{{$items['data'][0]->AccountCode}}</td>
-            <td>{{$items['data'][0]->Name}}</td>
-            <td>{{ $carbon::parse($item->TransactionDate)->format('d/m/Y') }}</td>
-            <td>{!!$item->voucher_linked!!}</td>
-            <td>{{$item->RefNo}}</td>
-            <td>{{$item->Description}}</td>
-            <td>{{ strtoupper($item->currency->code) }}</td>
-            <td>
-                {{ (($item->Debit != 0)? $item->Debit: $item->Credit) / $item->rate, 2 }}
-            </td>
-            <td>{{$item->rate}}</td>
-            <td>{{$item->Debit}}</td>
-            <td>{{($item->Credit * -1)}}</td>
-            <td>{{$item->endingBalance}}</td>
-            <td>{{$item->project_number}}</td>
-            <td>{{$item->po_number}}</td>
+            <td></td>
+            <td>Total</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>{{ $total_foreign }}</td>
+            <td></td>
+            <td>{{ $total_debit }}</td>
+            <td>{{ -$total_credit }}</td>
+            <td>{{ $total_ending_balance }}</td>
+            <td></td>
+            <td></td>
         </tr>
-        @endforeach
-    @endforeach
-      <tr>
-        <td>{{ $number }}</td>
-        <td></td>
-        <td>Total</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td>{{ $total_foreign }}</td>
-        <td></td>
-        <td>{{ $total_debit }}</td>
-        <td>{{ -$total_credit }}</td>
-        <td>{{ $total_ending_balance }}</td>
-        <td></td>
-        <td></td>
-      </tr>
-  </tbody>
+    </tbody>
 </table>
